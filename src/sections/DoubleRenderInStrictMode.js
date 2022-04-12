66732edfb8186b1043b8e28b9a905912712c7a42
Mislav Miocevic <mislav.miocevic@infinum.com>
2022-04-12 11:55:48 +0200

    Edit Double render in strict mode section

--- src/sections/DoubleRenderInStrictMode.js
+++ src/sections/DoubleRenderInStrictMode.js
@@ -20,7 +20,7 @@ export const DoubleRenderInStrictMode = () => {
         fetch('https://run.mocky.io/v3/13c885e1-2785-455b-8279-2766ca983157')
             .then(response => response.json())
             .then(data => console.log(data));
-    }, [])
+    }, []);
 
     useEffect(() => {
         // {"todos": ["learn React 18", "turn off strict mode???"]}
@@ -30,16 +30,33 @@ export const DoubleRenderInStrictMode = () => {
                 setTodos(prevState => [...new Set([...prevState, ...data.todos])]);
                 setBuggyTodos(prevState => [...prevState, ...data.todos]);
             });
-    }, [])
+
+        return () => {
+            setTodos(['original todo']);
+        }
+    }, []);
+
+    const onClick = () => {
+        // {"todos": ["NEW learn React 18", "NEW turn off strict mode???"]}
+        fetch('https://run.mocky.io/v3/1b17eeeb-9f63-4ad1-ae0f-2911acc98643')
+            .then(response => response.json())
+            .then(data => {
+                setTodos(prevState => [...new Set([...prevState, ...data.todos])]);
+                setBuggyTodos(prevState => [...prevState, ...data.todos]);
+            });
+    };
 
     return (
         <Layout title={doubleRenderInStrictModeTitle} previousSectionTitle={migrationTitle} nextSectionTitle={useIdHookTitle}>
             <p>More details about the double render in the strict more can be found in
                 the <a href="https://github.com/reactwg/react-18/discussions/96">GH discussion thread</a> and in the <a href="https://github.com/facebook/react/issues/15074#issuecomment-611998431">
                     GH issue thread</a>.</p>
+            <p>You can also read more about state handling
+                and the strict mode in the <a href="https://github.com/reactwg/react-18/discussions/18">GH discusson thread</a>.</p>
 
             <p style={{ marginTop: '64px' }} className="example">Example - useEffect</p>
             <p>[I am initially 0, and I only have <code>useEffect</code> with no arguments which sets a state to (0 + 1):]</p>
+            <p>There is also a fetcher call in <code>useEffect</code>. Open console to see the logs and network.</p>
             <p>Render count: {renderCount}</p>
 
             <p className="example-code">Example code - useEffect</p>
@@ -47,6 +64,10 @@ export const DoubleRenderInStrictMode = () => {
 
 useEffect(() => {
     setRenderCount(prevValue => prevValue + 1);
+    
+    fetch('https://run.mocky.io/v3/13c885e1-2785-455b-8279-2766ca983157')
+        .then(response => response.json())
+        .then(data => console.log(data));
 }, []);
 
 <p>Render count: {renderCount}</p>`} />
@@ -61,6 +82,7 @@ useEffect(() => {
             <img style={{ width: '100%', border: '10px solid black', boxSizing: 'border-box' }} alt="Double render in strict mode for production and local environment" src={doubleRenderImage} />
 
             <p style={{ marginTop: '64px' }} className="example">Example - list</p>
+            <button onClick={onClick}>Load more todos</button>
             <p>Todos:</p>
             {todos.map(todo => (
                 <div key={todo}>{todo}</div>
@@ -83,17 +105,37 @@ useEffect(() => {
             setTodos(prevState => [...new Set([...prevState, ...data.todos])]);
             setBuggyTodos(prevState => [...prevState, ...data.todos]);
         });
-}, [])
+    
+    return () => {
+        setTodos(['original todo']);
+    }
+}, []);
 
-<p>Todos:</p>
-{todos.map(todo => (
-    <div key={todo}>{todo}</div>
-))}
+const onClick = () => {
+    // {"todos": ["NEW learn React 18", "NEW turn off strict mode???"]}
+    fetch('https://run.mocky.io/v3/1b17eeeb-9f63-4ad1-ae0f-2911acc98643')
+        .then(response => response.json())
+        .then(data => {
+            setTodos(prevState => [...new Set([...prevState, ...data.todos])]); // here Set might be optional
+            setBuggyTodos(prevState => [...prevState, ...data.todos]);
+        });
+};
 
-<p>Buggy todos:</p>
-{buggyTodos.map((todo, index) => (
-    <div key={todo + index}>{todo}</div>
-))}
+return (
+    <>
+        <button onClick={onClick}>Load more todos</button>
+        
+        <p>Todos:</p>
+        {todos.map(todo => (
+            <div key={todo}>{todo}</div>
+        ))}
+        
+        <p>Buggy todos:</p>
+        {buggyTodos.map((todo, index) => (
+            <div key={todo + index}>{todo}</div>
+        ))}
+    </>
+);
 `} />
         </Layout>
     )
